1efc729766019316b67b53dc984e03c5a1301df6
Yebbenbe <lmtpox@gmail.com>
2024-01-08 20:49:27 -0400

    add ejs templates, some route base code

---
+++ routes/wikimaps.js
@@ -0,0 +1,24 @@
+
+/*  ASK Dustin abt this
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into /users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+module.exports = router;
